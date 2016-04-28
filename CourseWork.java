1386c91423cf864a41f15bc19f2fbbef0b878c14
Etooluwa <etosegun@gmail.com>
2016-04-27 23:08:24 -0400

    Update CourseWork.java

--- CourseWork.java
+++ CourseWork.java
@@ -1,4 +1,21 @@
 
 public class CourseWork {
+	private String name;
+	private int[] date;
+	private int perentage;
+
+	public String getCourseName(){
+		return name;
+	}
+
+	public int[] getDate(){
+		return date;
+	}
+	
+	public int getPercentage (){
+		return this.perentage;
+	}
+	
+	
 
 }
