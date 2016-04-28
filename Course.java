1fec8eda72522f6f95faa66e29d09f28d93ef938
Etooluwa <etosegun@gmail.com>
2016-04-27 23:09:33 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,4 +1,40 @@
+import java.util.ArrayList;
 
 public class Course {
+	private ArrayList<CourseWork> coursework;
+	private String courseCode;
+	private String instructor;
+	private int mark;
+	private Course prerequisite;
+	
+	public ArrayList<CourseWork> getCWork (){
+		return coursework;
+	}
+	public String getCourseCode (){
+		return courseCode;
+	}
+	public String getInstructor (){
+		return instructor;
+	}
+	public int getMark (){
+		return mark;
+	}
+    
+	public void setMark(int theMark){
+		mark= theMark;
+	}
+	public boolean passed (){    //method determines if one passes the course or not
+		if (mark < 60){
+			return false;
+		}
+		else return true;
+	}
+	public boolean hasPrereq (){    //this method determines whether the method has a prerequisite course
+		if (this.prerequisite!= null){
+			return true;
+		}
+		else return false;
+	}
+	
 
 }
