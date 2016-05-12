56d2ee8b61b4bfe0881a46140f364e31bebd2e83
Etooluwa <etosegun@gmail.com>
2016-05-12 16:26:23 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -6,6 +6,7 @@ public class Course {
 	private String instructor;
 	private int mark;
 	private Course prerequisite;
+	private Course nextCourse;
 	
 	public ArrayList<CourseWork> getCWork (){
 		return coursework;
@@ -23,18 +24,74 @@ public class Course {
 	public void setMark(int theMark){
 		mark= theMark;
 	}
+	public void setNextCourse(Course theCourse){
+		nextCourse = theCourse;
+	}
+	public void setPreReq(Course theCourse){
+		this.prerequisite = theCourse;
+	}
+	
 	public boolean passed (){    //method determines if one passes the course or not
 		if (mark < 60){
 			return false;
 		}
 		else return true;
 	}
-	public boolean hasPrereq (){    //this method determines whether the method has a prerequisite course
+	public boolean hasPrereq (){    //this method determines whether the course has a prerequisite course
 		if (this.prerequisite!= null){
 			return true;
 		}
 		else return false;
 	}
+	public boolean isPrereq (){    //this method determines whether the course is a prerequisite course
+		if (this.nextCourse!= null){
+			return true;
+		}
+		else return false;
+	}
+	
+	public String theGrade (){
+		if (mark >89 && mark <101){
+			return "A+";
+		}
+		else if (mark <90 && mark > 84){
+			return "A";
+		}
+		else if (mark <85 && mark > 79){
+			return "A-";
+		}
+		else if (mark <80 && mark > 76){
+			return "B+";
+		}
+		else if (mark <77 && mark > 72){
+			return "B";
+		}
+		else if (mark <73 && mark > 69){
+			return "B-";
+		}
+		else if (mark <70 && mark > 66){
+			return "C+";
+		}
+		else if (mark <67 && mark > 62){
+			return "C";
+		}
+		else if (mark <63 && mark > 59){
+			return "C-";
+		}
+		else if (mark <60 && mark > 56){
+			return "D+";
+		}
+		else if (mark <57 && mark > 52){
+			return "D";
+		}
+		else if (mark <53 && mark > 49){
+			return "D-";
+		}
+		else 
+			return "F";
+		
+	}
+	
 	
 
 }
